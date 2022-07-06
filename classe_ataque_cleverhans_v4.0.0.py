4142d120cada0b74ed6d0a9a033f7e0668f5aede
DianaRMagalhaes <diana.magalhaes@dtx-colab.pt>
2022-07-06 17:50:29 +0100

    Update

--- classe_ataque_cleverhans_v4.0.0.py
+++ classe_ataque_cleverhans_v4.0.0.py
@@ -6,13 +6,15 @@ import os
 import importlib
 import sys
 import torch.optim as optim
+from pretty_confusion_matrix import pp_matrix_from_data
 
+import pandas as pd
 import numpy as np
 import time
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from typing import Union
-
+from pretty_confusion_matrix import pp_matrix
 import torch
 from torchvision import datasets
 from torch.autograd import Variable
@@ -32,6 +34,9 @@ from torch_ava.data.gen_dataset_loader import LoaderOperator
 from torch_ava.engine.evaluator import Evaluator
 from torch_ava.torch_utils.operators import ModelOperator
 from models.Demo.torch_model import Network
+from sklearn.metrics import confusion_matrix
+from pretty_confusion_matrix import pp_matrix
+
 
 
 def set_module_import(model_name):
@@ -79,6 +84,9 @@ class Ataque:
         test_dl = x.get_loader(mode=test, torch_dset=path, batch_size=50)
 
         return train_dl, test_dl
+    
+    def new_y_pred(y_true, y_pred):
+        return 1*(y_true == y_pred)
 
     def load_dataset(json_confs: dict, path_dataset: str = "./MedNIST/"):
         # Dataset
@@ -129,6 +137,9 @@ class Ataque:
         adva_eps = 0.3
 
         model.eval()
+        y_true_all=[]
+        y_pred_all=[]
+        y_pred_attack=[]
         report = EasyDict(nb_test=0, correct=0, correct_fgm=0, correct_pgd=0, correct_carliniwagnerl2=0, correct_sparse=0, correct_lbfgs=0, correct_hop=0)
         for x, y in val_loader:
             x, y = x.to(model_operator.get_device()), y.to(model_operator.get_device())
@@ -137,29 +148,48 @@ class Ataque:
             #x_sparse = sparse_l1_descent(model, x)
             #x_carliniwagnerL2 = carlini_wagner_l2(model, x, n_classes=10)
             #x_lbfgs = LBFGS(model)
-            x_hop = hop_skip_jump_attack(model, x, np.inf)
+            #x_hop = hop_skip_jump_attack(model, x, np.inf)
             _, y_pred = model(x).max(1)  # model prediction on clean examples
             _, y_pred_fgm = model(x_fgm).max(1)  # model prediction on FGM adversarial examples
             #_, y_pred_pgd = model(x_pgd).max(1)  # model prediction on PGD adversarial examples
             #_, y_pred_carliniwagnerl2 = model(x_carliniwagnerL2).max(1)
             #_, y_pred_sparse = model(x_sparse).max(1) # model prediction on Sparse L1 Descent adversarial examples
             #_, y_pred_lbfgs = model(x_lbfgs).max(1) # model prediction on LBFGS adversarial examples
-            _, y_pred_hop = model(x_hop).max(1) #model prediction on Hop Skip Jump adversarial examples
+            #_, y_pred_hop = model(x_hop).max(1) #model prediction on Hop Skip Jump adversarial examples
             report.nb_test += y.size(0)
             report.correct += y_pred.eq(y).sum().item()
-            #report.correct_fgm += y_pred_fgm.eq(y).sum().item()
+            report.correct_fgm += y_pred_fgm.eq(y).sum().item()
             #report.correct_pgd += y_pred_pgd.eq(y).sum().item()
             #report.correct_carliniwagnerl2 += y_pred_carliniwagnerl2.eq(y).sum().item()
             #report.correct_lbfgs += y_pred_lbfgs.eq(y).sum().item()
             #report.correct_sparse += y_pred_sparse.eq(y).sum().item()
-            report.correct_hop += y_pred_hop.eq(y).sum().item()
+            #report.correct_hop += y_pred_hop.eq(y).sum().item()
+            y_true_all.append(np.array(y))
+            y_pred_all.append(np.array(y_pred))
+            y_pred_attack.append(np.array(y_pred_fgm))
         print("test acc on clean examples (%): {:.3f}".format(report.correct / report.nb_test * 100.0))
-        #print("test acc on FGM adversarial examples (%): {:.3f}".format(report.correct_fgm / report.nb_test * 100.0))
+        print("test acc on FGM adversarial examples (%): {:.3f}".format(report.correct_fgm / report.nb_test * 100.0))
         #print("test acc on PGD adversarial examples (%): {:.3f}".format(report.correct_pgd / report.nb_test * 100.0))
         #print("test acc on Carlini Wagner L2 adversarial examples (%): {:.3f}".format(report.correct_carliniwagnerl2 / report.nb_test * 100.0))
         #print("test acc on Sparse L1 Descent adversarial examples (%): {:.3f}".format(report.correct_sparse / report.nb_test * 100.0))
         #print("test acc on LBFGS adversarial examples (%): {:.3f}".format(report.correct_lbfgs / report.nb_test * 100.0))
-        print("test acc on Hop Skip Jump adversarial examples (%): {:.3f}".format(report.correct_hop / report.nb_test * 100.0))
+        #print("test acc on Hop Skip Jump adversarial examples (%): {:.3f}".format(report.correct_hop / report.nb_test * 100.0))
+        
+        y_true = np.concatenate(np.array(y_true_all), axis=0)
+        y_pred_v2= np.concatenate(np.array(y_pred_all), axis=0)
+
+        print(y_true)
+        print(y_pred_v2)
+#        print(np.ones(len(y_true)))
+#        print(new_y_pred(y_true, y_pred_v2))
+        #cm_array = confusion_matrix(y_true,y_pred_v2)
+        cm_array = confusion_matrix(np.ones(len(y_true)), 1*(y_true == y_pred_v2))
+            # meter em pandas dataframe para poderes escolher o nome das classes
+        classes_list = ['class1', 'class2']
+        df = pd.DataFrame(cm_array)
+            # colormap: fica aqui o link https://matplotlib.org/stable/tutorials/colors/colormaps.html
+        cmap = 'Blues'
+        pp_matrix(df, cmap=cmap) 
 
 if __name__ == "__main__":
 
