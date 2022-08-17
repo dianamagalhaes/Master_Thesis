dd9665b0f5bf728b4160acd1b61e81585ab3fef7
Adriano <apinto@ai4medimaging.com>
2022-08-17 10:05:11 +0100

    Code refactor, improved local imports and paths

--- classe_ataque_cleverhans_v4.0.0.py
+++ classe_ataque_cleverhans_v4.0.0.py
@@ -6,6 +6,8 @@ import joblib
 import os
 import importlib
 import sys
+import glob
+import argparse
 
 
 import numpy as np
@@ -25,25 +27,28 @@ from cleverhans.torch.attacks.projected_gradient_descent import projected_gradie
 from cleverhans.torch.attacks.carlini_wagner_l2 import carlini_wagner_l2
 from cleverhans.torch.attacks.sparse_l1_descent import sparse_l1_descent
 from cleverhans.torch.attacks.hop_skip_jump_attack import hop_skip_jump_attack
-#from cleverhans.torch.attacks.lbfgs import LBFGS
+
+# from cleverhans.torch.attacks.lbfgs import LBFGS
 from torch_ava.data.get_transformations import DataAugOperator
-from torch_ava.engine.evaluator import Evaluator
-from torch_ava.data import MedNISTDataset
-from torch_ava.data.gen_dataset_loader import LoaderOperator
-from torch_ava.engine.evaluator import Evaluator
-from torch_ava.torch_utils.operators import ModelOperator
+from torch_ava.data import MedNISTDataset, LoaderOperator
+from torch_ava.torch_utils import TensorboardLoggerOperator, ModelOperator
+from torch_ava.engine.trainer import Trainer
 
 
-def set_module_import(model_name):
+def set_module_import(model_name: str):
+    """This utility function imports the model folder as an additional module of this script.
+    Allowing it to gather all the necessary information for train/val/test and also to attack the model.
 
+    Args:
+        model_name (str): String that matches the codename of the model ought to be imported as a module.
+    """
     if "ava_model" in sys.modules.keys():
         submodules = [mod for mod in sys.modules.keys() if mod.startswith("ava_model.")]
         del sys.modules["ava_model"]
         for submodule in submodules:
             del sys.modules[submodule]
 
-    module_path = os.path.join(f"./models/{model_name}", "__init__.py")
-    module_path = os.path.abspath(module_path)
+    module_path = os.path.abspath(os.path.join(f"./models/{model_name}", "__init__.py"))
 
     spec = importlib.util.spec_from_file_location("ava_model", module_path)
 
@@ -52,47 +57,60 @@ def set_module_import(model_name):
     spec.loader.exec_module(module)
 
 
-def get_configs(json_file_path: str) -> dict:
+def get_model_configs(json_file_path: str) -> dict:
 
     with open(json_file_path) as f:
-        json_confs = json.load(f)
-        return json_confs
+        return json.load(f)
 
 
 class Ataque:
-    def __init__(self, model_weights_path: str, dataset_path: str = "./MedNIST/"):
 
-        self.model_weights_path = model_weights_path
+    adversarial_atacks = {
+        "Fast Gradient Method": {"call": fast_gradient_method, "kwargs": {"eps": 0.3, "norm": np.inf}},
+        "Projected Gradient Descent": {
+            "call": projected_gradient_descent,
+            "kwargs": {"eps": 0.3, "eps_iter": 0.01, "nb_iter": 40, "norm": np.inf},
+        },
+        "Sparse L1 Descent": {"call": sparse_l1_descent, "kwargs": {}},
+        "Carlini Wagner L2": {"call": carlini_wagner_l2, "kwargs": {"n_classes": 6}},
+        "Hop Skip Jump": {"call": hop_skip_jump_attack, "kwargs": {"norm": np.inf}},
+    }
+
+    def __init__(self, json_confs: dict, dataset_path: str = "./MedNIST/"):
+
+        self.json_confs = json_confs
         self.dataset_path = dataset_path
 
-    def load_dataset(self, json_confs: dict):
-        # --- Dataset
+    @staticmethod
+    def _get_loader(mode: str, main_data_loader: object, main_torch_dset: object, json_confs: dict):
 
-        # Load Data Transformation Pipelines
-        train_data_aug = DataAugOperator()
-        val_data_aug = DataAugOperator()
+        data_trfm = DataAugOperator()
+        data_trfm.set_pipeline(json_confs[mode]["transformations"])
+        torch_dset = MedNISTDataset(main_torch_dset, data_trfm.get_pipeline())
+        torch_loader = main_data_loader.get_loader(mode, torch_dset, json_confs[mode]["batch_size"])
 
-        train_data_aug.set_pipeline(json_confs["train"]["transformations"])
-        val_data_aug.set_pipeline(json_confs["val"]["transformations"])
+        return torch_dset, torch_loader
+
+    def load_dataset(self):
 
         # Load Dataset
         mednist_data = datasets.ImageFolder(root=self.dataset_path)
-        train_data = MedNISTDataset(mednist_data, train_data_aug.get_pipeline())
-        val_data = MedNISTDataset(mednist_data, val_data_aug.get_pipeline())
-        data_loader = LoaderOperator(train_data)
-        torch_train_loader = data_loader.get_loader("train", train_data, json_confs["train"]["batch_size"])
-        torch_val_loader = data_loader.get_loader("val", val_data, json_confs["val"]["batch_size"])
+        self.labels_to_idx = mednist_data.class_to_idx
+        data_loader = LoaderOperator(mednist_data)
 
-        # Display image and label.
+        torch_dset, torch_train_loader = self._get_loader("train", data_loader, mednist_data, self.json_confs)
+        _, torch_val_loader = self._get_loader("val", data_loader, mednist_data, self.json_confs)
+        _, torch_test_loader = self._get_loader("test", data_loader, mednist_data, self.json_confs)
+
         train_features, _ = next(iter(torch_train_loader))
         print(f"Feature batch shape: {train_features.size()}")
-        return torch_train_loader, torch_val_loader
+        return torch_dset, torch_train_loader, torch_val_loader, torch_test_loader
 
-    def load_torch_model(self):
+    def load_torch_model(self, model_weights_path: str):
 
-        model = torch.load(self.model_weights_path)
+        model = torch.load(model_weights_path)
 
-        if self.model_weights_path.endswith(".pth"):
+        if model_weights_path.endswith(".pth"):
             print("The model saving only considered the dictionary of weights. Not the whole model.")
 
             torch_model.model.load_state_dict(model)
@@ -103,25 +121,32 @@ class Ataque:
         return model
 
     @staticmethod
-    def set_model(train_data, torch_train_loader):
-        ch, w, h = train_data[0][0].shape
+    def set_model_to_train(torch_train_dset, torch_train_loader):
+        ch, w, h = torch_train_dset[0][0].shape
         inpt_dims = [torch_train_loader.batch_size, ch, w, h]
         print("Input Dimensions:", inpt_dims)
-        model = torch_model.Network(inpt_dims)
+        if hasattr(torch_model, "Network"):
+            model = torch_model.Network(inpt_dims)
+        else:
+            model = torch_model.model
+
         optimizer, scheduler = torch_model.get_optimizer(model)
         print(model)
 
         return model, optimizer, scheduler
 
-    @staticmethod
-    def train_model(model, torch_train_loader, optimizer):
-        # TODO add the possibility to define which hardware will run the train
-        # Due to lack of hardware use_cuda must be False
+    def train_model(self, model, device, torch_train_loader, torch_val_loader, optimizer, scheduler):
 
-        model_operator = ModelOperator(use_cuda=False)
+        model_operator = ModelOperator(device=device)
         model_operator.set_loss(torch_model.loss)
         model_operator.set_optimizer(optimizer)
         model.to(model_operator.get_device())
+
+        model_dir = os.path.dirname(ava_model.__file__)
+        logger = TensorboardLoggerOperator(model_dir, labels_index=self.labels_to_idx)
+
+        trainer_operator = Trainer(model_operator, logger, epochs=self.json_confs["train"]["epochs"])
+        trainer_operator.run_epochs(model, torch_train_loader, torch_val_loader, scheduler)
 
         # Train vanilla model
         model.train()
@@ -138,109 +163,107 @@ class Ataque:
                 train_loss += loss.item()
             print("epoch: {}/{}, train loss: {:.3f}".format(epoch, 1, train_loss))
 
-    @staticmethod
-    def eval(model, torch_val_loader):
+    def eval_attack(self, model: object, attack_name: str, torch_loader: object, device: str):
         # Evaluate on clean and adversarial data
 
-        model_operator = ModelOperator(use_cuda=False)
+        model_operator = ModelOperator(device=device)
         device = model_operator.get_device()
         model.to(device)
-
-        # Total eps of the adversarial attack
-        # TODO in the future we might want to explore this variable, thus we need to make it configurable
-        adva_eps = 0.3
-
         model.eval()
 
         y_true = []
         y_pred = []
         with torch.no_grad():
-            for x_val, y_val in torch_val_loader:
+            for x_val, y_val in torch_loader:
                 data, labels = (
                     x_val.to(device),
                     y_val.to(device),
                 )
+
                 pred = model(data).argmax(dim=1)
                 for i in range(len(pred)):
                     y_true.append(labels[i].item())
                     y_pred.append(pred[i].item())
 
-        print(
-            classification_report(
-                y_true, y_pred, target_names=["AbdomenCT", "BreastMRI", "CXR", "ChestCT", "Hand", "HeadCT"], digits=4
-            )
-        )
-        report = EasyDict(
-            nb_test=0,
-            correct=0,
-            correct_fgm=0,
-            correct_pgd=0,
-            correct_carliniwagnerl2=0,
-            correct_sparse=0,
-            correct_lbfgs=0,
-            correct_hop=0,
-        )
-        for x, y in torch_val_loader:
+        if attack_name in Ataque.adversarial_atacks:
+            adva_atack = Ataque.adversarial_atacks[attack_name]
+
+        else:
+            raise ValueError("Please provide a valid and supported Attack")
+        y_pred_attacked = []
+
+        for x, y in torch_loader:
             x, y = x.to(device), y.to(device)
-            x_fgm = fast_gradient_method(model, x, adva_eps, np.inf)
-            #x_pgd = projected_gradient_descent(model, x, adva_eps, 0.01, 40, np.inf)
-            #x_sparse = sparse_l1_descent(model, x)
-            #x_carliniwagnerL2 = carlini_wagner_l2(model, x, n_classes=6)
-            #x_lbfgs = LBFGS(model)
-            #x_hop = hop_skip_jump_attack(model, x, np.inf)
-            _, y_pred = model(x).max(1)  # model prediction on clean examples
-            _, y_pred_fgm = model(x_fgm).max(1)  # model prediction on FGM adversarial examples
-            #_, y_pred_pgd = model(x_pgd).max(1)  # model prediction on PGD adversarial examples
-            #_, y_pred_carliniwagnerl2 = model(x_carliniwagnerL2).max(1)
-            #_, y_pred_sparse = model(x_sparse).max(1) # model prediction on Sparse L1 Descent adversarial examples
-            #_, y_pred_lbfgs = model(x_lbfgs).max(1) # model prediction on LBFGS adversarial examples
-            #_, y_pred_hop = model(x_hop).max(1)  # model prediction on Hop Skip Jump adversarial examples
-            report.nb_test += y.size(0)
-            report.correct += y_pred.eq(y).sum().item()
-            report.correct_fgm += y_pred_fgm.eq(y).sum().item()
-            #report.correct_pgd += y_pred_pgd.eq(y).sum().item()
-            #report.correct_carliniwagnerl2 += y_pred_carliniwagnerl2.eq(y).sum().item()
-            #report.correct_lbfgs += y_pred_lbfgs.eq(y).sum().item()
-            #report.correct_sparse += y_pred_sparse.eq(y).sum().item()
-            #report.correct_hop += y_pred_hop.eq(y).sum().item()
-        print("test acc on clean examples (%): {:.3f}".format(report.correct / report.nb_test * 100.0))
-        print("test acc on FGM adversarial examples (%): {:.3f}".format(report.correct_fgm / report.nb_test * 100.0))
-        #print("test acc on PGD adversarial examples (%): {:.3f}".format(report.correct_pgd / report.nb_test * 100.0))
-        #print("test acc on Carlini Wagner L2 adversarial examples (%): {:.3f}".format(report.correct_carliniwagnerl2 / report.nb_test * 100.0))
-        #print("test acc on Sparse L1 Descent adversarial examples (%): {:.3f}".format(report.correct_sparse / report.nb_test * 100.0))
-        #print("test acc on LBFGS adversarial examples (%): {:.3f}".format(report.correct_lbfgs / report.nb_test * 100.0))
-        #print("test acc on Hop Skip Jump adversarial examples (%): {:.3f}".format(report.correct_hop / report.nb_test * 100.0))
+            x_attacked = adva_atack["call"](
+                model, x, **adva_atack["kwargs"]
+            )  # fast_gradient_method(model, x, adva_eps, np.inf)
+            pred_attacked = model(x_attacked).cpu().data.numpy().argmax(axis=1).tolist()
+            y_pred_attacked += pred_attacked
+
+        print(
+            "\n Classification Performance on clean samples: \n",
+            classification_report(y_true, y_pred, target_names=self.json_confs["target_names"], digits=4),
+        )
+
+        print(
+            f"\n Classification Performance on {attack_name} Adversarial Attack: \n",
+            classification_report(y_true, y_pred_attacked, target_names=self.json_confs["target_names"], digits=4),
+        )
 
 
 if __name__ == "__main__":
 
-    import platform
+    # Package configurations
+    pckg_confs = open("pckg_confs.json")
+    pckg_confs = json.load(pckg_confs)
 
-    model_name = "Demo"
-    # Since this model is in pth, only a dictionary of the weights are store not the whole model!
-    model_weights_path = f"models/{model_name}/LOGS/models/nnet_epoch_9.pt"
+    # Check for gpu availability
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+    cuda_devices = torch.cuda.device_count()
+    cuda_codenames = [f"cuda:{idx}" for idx in range(cuda_devices)]
 
-    if platform.node() == "nea138-lt":  # Adriano Pinto's machine name
-        json_file_path = f"/home/apinto/Documents/projects/msc_thesis_DianaMag/models/{model_name}/configs.json"
-    else:
-        json_file_path = f"/home/diana-dtx/Desktop/Master_Thesis/models/{model_name}/configs.json"
-    model = {}
-    train = None
-    train_dl = None
-    test_dl = None
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-m",
+        "--model_name",
+        required=True,
+        type=str,
+        help="Name of the model project folder that will be used in the study.",
+    )
+    parser.add_argument(
+        "--device",
+        required=True,
+        type=str,
+        default="cpu",
+        choices=["cpu"] + cuda_codenames,
+        help="Device that will execute the script. Defaults to CPU if not specified, "
+        "if the user provides a gpu id and there is None available.",
+    )
+    args = parser.parse_args()
+
+    # model_name = "MedNIST_Dense121_MONAI"
 
     # Let's start the engine environment
-    set_module_import(model_name)
+    set_module_import(args.model_name)
 
     import ava_model
     from ava_model import torch_model
 
-    json_confs = get_configs(json_file_path)
+    model_configs_path = os.path.abspath(os.path.join(pckg_confs["MODELS_DIR"], f"{args.model_name}/configs.json"))
+    model_configs = get_model_configs(model_configs_path)
 
-    ataque = Ataque(model_weights_path=model_weights_path)
+    ataque = Ataque(json_confs=model_configs)
+    torch_dset, torch_train_loader, torch_val_loader, torch_test_loader = ataque.load_dataset()
+
+    # model, optim, _ = ataque.set_model_to_train(torch_dset, torch_train_loader=torch_train_loader)
+    # ataque.train_model(model, args.device, torch_train_loader, torch_val_loader, optim, None)
 
     # Pipeline for an already trained model
 
-    model = ataque.load_torch_model()
-    _, torch_val_loader = ataque.load_dataset(json_confs=json_confs)
-    ataque.eval(model, torch_val_loader)
+    # We will assume that there is only one best model
+    # Note that the torch models can either be with .pt or .pth extension.
+    # For .pth models, only a dictionary of the weights are store not the whole model!
+    model_weights_path = glob.glob(f"models/{args.model_name}/LOGS/models/best*")[0]
+
+    model = ataque.load_torch_model(model_weights_path)
+    ataque.eval_attack(model, "Fast Gradient Method", torch_val_loader, device=args.device)
