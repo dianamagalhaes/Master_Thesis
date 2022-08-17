6166963ae5e73740cad25b3e994fbb88be5626eb
Adriano <apinto@ai4medimaging.com>
2022-08-17 10:11:04 +0100

    Configuration for DenseNet121

--- models/MedNIST_Dense121_MONAI/torch_model.py
+++ models/MedNIST_Dense121_MONAI/torch_model.py
@@ -5,12 +5,10 @@ from torch.optim.lr_scheduler import StepLR
 from monai.networks.nets import DenseNet121
 
 
-
-
 # Training configs
 def get_optimizer(model):
 
-    optimizer = optim.Adadelta(model.parameters(), lr=1e-5)
+    optimizer = optim.Adam(model.parameters(), lr=1e-5)
     scheduler = StepLR(optimizer, step_size=1, gamma=0.7)
     return optimizer, scheduler
 
