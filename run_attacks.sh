862737c77aa06b0898cd1141d6d2066c2dd3af84
Adriano <apinto@ai4medimaging.com>
2022-10-08 17:05:24 +0100

    Change configurations to allow Carlini Wagner L2 attack

--- run_attacks.sh
+++ run_attacks.sh
@@ -1,5 +1,5 @@
-python classe_ataque_cleverhans_v4.0.0.py -m Demo --device cuda:0 eval --attack Fast_Gradient_Method
-python classe_ataque_cleverhans_v4.0.0.py -m Demo --device cuda:0 eval --attack Projected_Gradient_Descent
-python classe_ataque_cleverhans_v4.0.0.py -m Demo --device cuda:0 eval --attack Sparse_L1_Descent
+#python classe_ataque_cleverhans_v4.0.0.py -m Demo --device cuda:0 eval --attack Fast_Gradient_Method
+#python classe_ataque_cleverhans_v4.0.0.py -m Demo --device cuda:0 eval --attack Projected_Gradient_Descent
+#python classe_ataque_cleverhans_v4.0.0.py -m Demo --device cuda:0 eval --attack Sparse_L1_Descent
 python classe_ataque_cleverhans_v4.0.0.py -m Demo --device cuda:0 eval --attack Carlini_Wagner_L2
-python classe_ataque_cleverhans_v4.0.0.py -m Demo --device cuda:0 eval --attack Hop_Skip_Jump
+#python classe_ataque_cleverhans_v4.0.0.py -m Demo --device cuda:0 eval --attack Hop_Skip_Jump
